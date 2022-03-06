f1dadd97504e3fe65a6f73a49d2245aa5e3a47c5
nara113 <0554my@naver.com>
2022-03-07 00:58:59 +0900

    프록시 패턴과 데코레이 패턴 - 예제 프로젝트 만들기 v2

--- src/main/java/hello/proxy/app/v1/OrderControllerV1.java
+++ src/main/java/hello/proxy/app/v1/OrderControllerV1.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+//@Controller 또는 @RequestMapping이 있으면 스프링 컨트롤러로 인식
+//@Controller는 @Component가 있어서 스캔 대상이 되므로 RequestMapping사
 @RequestMapping
 @ResponseBody
 public interface OrderControllerV1 {
