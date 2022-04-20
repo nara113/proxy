ed10b42b6f64740c36e975a97eb2c1f716cfd199
nara113 <0554my@naver.com>
2022-04-20 22:22:40 +0900

    스프링 AOP 포인트컷 - within

---
+++ src/test/java/hello/aop/pointcut/WithinTest.java
@@ -0,0 +1,34 @@
+package hello.aop.pointcut;
+
+import hello.aop.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
+
+import java.lang.reflect.Method;
+
+public class WithinTest {
+    AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+    Method helloMethod;
+
+    @BeforeEach
+    void init() throws NoSuchMethodException {
+        helloMethod = MemberServiceImpl.class.getMethod("hello", String.class);
+    }
+
+    @Test
+    void withinExact() {
+        pointcut.setExpression("within(hello.aop.member.MemberServiceImpl)");
+        Assertions.assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class))
+                .isTrue();
+    }
+
+    @Test
+    void withinFalse() {
+        pointcut.setExpression("within(hello.aop.member.MemberService)");
+        Assertions.assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class))
+                .isFalse();
+    }
+}
+
