f0a98c1e229fa6fee16709163e3571425b6fe790
nara113 <0554my@naver.com>
2022-04-26 03:09:41 +0900

    스프링 AOP 포인트컷 - this, target

---
+++ src/test/java/hello/aop/pointcut/ThisTargetTest.java
@@ -0,0 +1,52 @@
+package hello.aop.pointcut;
+
+import hello.aop.member.MemberService;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Slf4j
+@Import(ThisTargetTest.ThisTargetAspect.class)
+@SpringBootTest(properties = "spring.aop.proxy-target-class=false") //JDK 동적 프록
+public class ThisTargetTest {
+    @Autowired
+    MemberService memberService;
+
+    @Test
+    void test() {
+        log.info("memberService proxy : {}", memberService.getClass());
+        memberService.hello("hi");
+    }
+
+    @Aspect
+    static class ThisTargetAspect {
+        @Around("this(hello.aop.member.MemberService)")
+        Object thisInterface(ProceedingJoinPoint joinPoint) throws Throwable {
+            log.info("[this-interface] {}", joinPoint.getSignature());
+            return joinPoint.proceed();
+        }
+
+        @Around("target(hello.aop.member.MemberService)")
+        Object targetInterface(ProceedingJoinPoint joinPoint) throws Throwable {
+            log.info("[target-interface] {}", joinPoint.getSignature());
+            return joinPoint.proceed();
+        }
+
+        @Around("this(hello.aop.member.MemberServiceImpl)")
+        Object thisImpl(ProceedingJoinPoint joinPoint) throws Throwable {
+            log.info("[this-impl] {}", joinPoint.getSignature());
+            return joinPoint.proceed();
+        }
+
+        @Around("target(hello.aop.member.MemberServiceImpl)")
+        Object targetImpl(ProceedingJoinPoint joinPoint) throws Throwable {
+            log.info("[target-impl] {}", joinPoint.getSignature());
+            return joinPoint.proceed();
+        }
+    }
+}
