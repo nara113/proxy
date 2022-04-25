5188bf02b5af6332863c2a68b10c1d10e907a94c
nara113 <0554my@naver.com>
2022-04-26 04:19:37 +0900

    스프링 AOP 실전예제 - 재시도 AOP

--- src/main/java/hello/aop/exam/ExamRepository.java
+++ src/main/java/hello/aop/exam/ExamRepository.java
@@ -1,20 +1,23 @@
 package hello.aop.exam;
 
+import hello.aop.exam.annotation.Retry;
 import hello.aop.exam.annotation.Trace;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class ExamRepository {
 
-    private static int seq = 1;
+    private static int seq = 0;
 
+    @Retry(2)
     @Trace
     public String save(String itemId) {
+        seq++;
+
         if (seq % 5 == 0) {
             throw new IllegalStateException("error");
         }
 
-        seq++;
         return "ok";
     }
 }
---
+++ src/main/java/hello/aop/exam/aop/RetryAspect.java
@@ -0,0 +1,33 @@
+package hello.aop.exam.aop;
+
+import hello.aop.exam.ExamService;
+import hello.aop.exam.annotation.Retry;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+
+@Slf4j
+@Aspect
+public class RetryAspect {
+
+//    @Around("@annotation(hello.aop.exam.annotation.Retry)")
+    @Around("@annotation(retry)")
+    public Object doRetry(ProceedingJoinPoint joinPoint, Retry retry) throws Throwable {
+        log.info("[retry] {}, {}", joinPoint.getSignature(), retry);
+        int maxCount = retry.value();
+
+        Exception exceptionHolder = null;
+
+        for (int retryCount = 0; retryCount <= maxCount; retryCount++) {
+            try {
+                log.info("[retry] tryCount = {} /{}", retryCount, maxCount);
+                return joinPoint.proceed();
+            } catch (Exception e) {
+                exceptionHolder = e;
+            }
+
+        }
+        throw exceptionHolder;
+    }
+}
